11ac7dd02c8679872b87f08bc809c16f162ac0d0
unknown <parthshah51999@gmail.com>
2019-05-17 11:48:53 +0530

    code refactor

--- src/toDoList.js
+++ src/toDoList.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 class ToDoList extends React.Component {
   constructor() {
-      super();
-      this.state = {
+      super(); //it calls react Component constructor.
+      this.state = { //state is a plain js object..init only in constructor
         items: [],
         value: ''
       };
@@ -12,29 +12,40 @@ class ToDoList extends React.Component {
   }
 
   addItem() {
-    let inputText = this.state.value;
+    let inputText = this.state.value; //object destructring
     this.state.items = [...this.state.items, inputText];
-    this.state.value = '';
+    //this.state.value = '';  dont change state variables directly... use setState instead
     this.setState({
       value : '',
       items : this.state.items
     });
   }
 
-  displayList(){
-    return (this.state.items.slice(0).reverse().map((item,i) => <li key={i}>{item}</li>));
+  displayList() {
+    return (<ul>{this.state.items.slice(0).reverse().map((item, i) => <li key={i}>{item}</li>)}</ul>);
   }
 
-  render() {
-    let items = this.state.items;
+  render() { //render is a react component method.
+    let items = this.state.items; // use object destructring
     return (
       <div>
         <div id="addTask">
-          <input className="input" size="40" type="text" placeholder="Enter Task" onChange={e => this.setState({ value: e.target.value })} value={this.state.value}/>
-          <button onClick={this.addItem} type="button" className="btn btn-primary">Add</button>
+          <input
+            className="input"
+            size="40"
+            type="text"
+            placeholder="Enter Task"
+            onChange={e => this.setState({ value: e.target.value })}
+            value={this.state.value}
+          />
+          <button
+            onClick={this.addItem}
+            type="button"
+            className="btn btn-primary">Add
+          </button>
         </div>
         <div id="TaskList">
-          <ul>{this.displayList()}</ul>
+          {this.displayList()}
         </div>
       </div>
     );
