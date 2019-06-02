4dccaffc04cf2f0de663517c458706de272cd7b7
parthshah51999 <parthshah51999@gmail.com>
2019-06-02 23:59:28 +0530

    adds eslint loader for code refactoring

--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -2,31 +2,39 @@ import React from 'react';
 
 export default class ToDoItem extends React.Component {
   removeItem(id) {
-    return this.props.removeItem(id);
+    const { removeItem } = this.props;
+    return removeItem(id);
   }
 
   editItem(id) {
-    return this.props.editItem(id);
+    const { editItem } = this.props;
+    return editItem(id);
   }
 
   render() {
+    const { searchResults } = this.props;
     return (
       <div>
         <ul id="taskList">
           {
-            this.props.searchResults.map((item, i) => {
+            searchResults.map((item, i) => {
               return (
-                <li key={i}>{item}
-                <button
-                  onClick={this.removeItem.bind(this, i)}
-                  type="button"
-                  className="btn btn-primary">Remove
-                </button>
-                <button
-                  onClick={this.editItem.bind(this, i)}
-                  type="button"
-                  className="btn btn-primary">Edit
-                </button>
+                <li key={item}>
+                  {item}
+                  <button
+                    onClick={this.removeItem.bind(this, i)}
+                    type="button"
+                    className="btn btn-primary"
+                  >
+                    Remove
+                  </button>
+                  <button
+                    onClick={this.editItem.bind(this, i)}
+                    type="button"
+                    className="btn btn-primary"
+                  >
+                    Edit
+                  </button>
                 </li>
               );
             })
